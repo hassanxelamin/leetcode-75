65eda9c58c65ad2fe8c5e3193dd32c2ae7c72815
hassanxelamin <hello@hassanelamin.co>
2024-02-15 10:27:13 -0800

    x

---
+++ string/1-reverse-string/index.py
@@ -0,0 +1,10 @@
+# “Cat”, return “taC”
+# “The Daily Byte”, return "etyB yliaD ehT”
+# “civic”, return “civic”
+
+def reverseString(str):
+  reverse_str = str[::-1]
+  print(reverse_str)
+  
+  
+reverseString("Cat")
